6eb4dfa1156d99a7825668721e67c003b7c32e27
Raghav Katta <Raghavkatta2005@gmail.com>
2025-05-30 03:42:15 +0530

    Feat: VoiceRecorder.jsx

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,3 +1,4 @@
+
 import bcrypt from 'bcrypt';
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
---
+++ Frontend/src/components/VoiceRecorder.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
+
+const VoiceRecorder = () => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [audioBlob, setAudioBlob] = useState(null);
+    const mediaRecorderRef = useRef(null);
+
+
+    const startRecording = async () => {
+        /* navigator.mediaDevices is basically mic permission */
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        /* MediaRecorder=Recording on */
+        const mediaRecorder = new MediaRecorder(stream)
+        mediaRecorderRef.current = mediaRecorder
+        const chunks = []
+        mediaRecorder.ondataavailable = (event) => {
+            chunks.push(event.data)
+        }
+        /* blob is basically a data type that is used to store video/image recordings from the browser which are not stored yet  */
+        mediaRecorder.onstop = () => {
+            const audioBlob = new Blob(chunks, { type: 'audio/wav' })
+            setAudioBlob(audioBlob)
+        }
+
+        mediaRecorder.start()
+        setIsRecording(true)
+        console.log("Starting recording...")
+    }
+
+    const stopRecording = () => {
+        mediaRecorderRef.current.stop()
+        setIsRecording(false)
+        console.log("Stopping recording...")
+    }
+
+    return (
+        <>
+            <button onClick={startRecording}>Start Recording</button>
+            <button onClick={stopRecording}>Stop Recording</button>
+            {audioBlob && (
+                <audio controls src={URL.createObjectURL(audioBlob)} />
+            )}
+
+        </>
+    )
+
+}
+export default VoiceRecorder;
